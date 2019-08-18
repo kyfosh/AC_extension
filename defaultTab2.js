890ab885e1e160ec0cfd7019859f573a3388cfa6
Kyra <42686210+kyfosh@users.noreply.github.com>
2019-08-18 11:00:02 -0700

    Update and rename defaultTab1.js to defaultTab2.js

--- defaultTab2.js
+++ defaultTab2.js
@@ -1,9 +1,11 @@
 function defaultTab() {
+
 document.getElementById("exit").addEventListener("click", function(event){
-    chrome.tabs.getCurrent(function(tab){
+    chrome.tabs.getCurrent(null,function(tab){
       chrome.tabs.update(tab.id, {
         url: "chrome-search://local-ntp/local-ntp.html"
       });
     });
  });
+
  }
