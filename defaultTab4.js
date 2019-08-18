804c854e8cfd1d3610536f17934c16c7d0f2d415
Kyra <42686210+kyfosh@users.noreply.github.com>
2019-08-18 11:11:06 -0700

    Update and rename defaultTab3.js to defaultTab4.js

--- defaultTab4.js
+++ defaultTab4.js
@@ -1,7 +1,7 @@
 function defaultTab() {
 
 document.getElementById("exit").addEventListener("click", function(event){
-    chrome.tabs.getCurrent(null,function(tab){
+    chrome.tabs.getSelected(null,function(tab){
       chrome.tabs.update(tab.id, {
         url: "chrome-search://local-ntp/local-ntp.html"
       });
@@ -9,4 +9,5 @@ document.getElementById("exit").addEventListener("click", function(event){
  });
 
  }
+
 z = defaultTab();
