8262812b80076eb9c5a57c883ed78c447fba82db
Kyra <42686210+kyfosh@users.noreply.github.com>
2019-08-21 11:57:42 -0700

    Update and rename bgpicH.js to bgpicH1.js

--- bgpicH1.js
+++ bgpicH1.js
@@ -6,12 +6,12 @@ var image = [
   "https://lh3.googleusercontent.com/vXwCt5b3s8lyjB6_fvPymG5Xc0ntGJoy0RA4gzNxwX08GT6lPt57PXzQsCBy1rVrZP12__nsp2VyW4Nxq3_3w3DpBhTXasjGcpoE68Obs8q7liZ_Wpmp-Bc12c1YB4zPVcYAK64rGQ=w2400",
   "https://lh3.googleusercontent.com/OqSDAgw9S0PpUADIHjRFk2wWrbJR_t6t9cZ4hiL9jnrfr5o0pVrHkVazsxh1UliruA7xK2ROUdC8Ru8wJNyLZatWonB5Zflc1bFpB9nSBFYvhUCTbitBAzhAuQTdcp7scRNEFXiYZw=w2400"
 ];
-  document.html.style.backgroundColor = "#000";
+  document.getElementById('html').style.backgroundColor = "#000";
   var ran = Math.random();
   var num = Math.round(3*ran);
 
-  document.html.style.backgroundImage = "url('"+ image[num] + "')";
-  document.html.style.backgroundSize = "auto auto";
+  document.getElementById('html').style.backgroundImage = "url('"+ image[num] + "')";
+  document.getElementById('html').style.backgroundSize = "auto auto";
   t = setTimeout(function(){myFunction()}, 3600000)
 }
 
