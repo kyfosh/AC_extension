6a1a66854378bc4afcd2b28ab4d96570a4dbc97b
Kyra <42686210+kyfosh@users.noreply.github.com>
2019-08-21 12:47:09 -0700

    Update and rename bgpic4-5.js to bgpic4-6.js

--- bgpic4-6.js
+++ bgpic4-6.js
@@ -1,9 +1,9 @@
 function myFunction() {
 var image = [
   "https://lh3.googleusercontent.com/I6Rcp2oJxpSZLiPpSUJMPSqhlzn1fEFBXSJVPStXCgj7VKQ0FRM66kVlyvB5-DRId4SMc1GiW3coHneu4eCm70-Z-antU26sLb4UEa1qrVveqyWwVfOQtqix-0zxjaayhlT6xHhqyw=w2400",
-  "https://lh3.googleusercontent.com/ENU2qidHPdXlGF-P-2EYXefhecB2ol5qQX86BH4Z5y8CMHR3YW84T2YCkzw8H1Vv1be1JvaEbvWMucXijWS0QxArLGB1e5Fc-nSsfFnlM4D54DfcXHkV-vkMTOqLIyHxoVx37bth7Q=w2400",
+  "https://lh3.googleusercontent.com/RU2o1nRB97VI19SESRyvPVRO-6N12sJFjiEz_s9HVPJfq6UCbsUYz5mTs1ilrMWsRlV0J1qhUTBs4YdesxkvZKOg4s10GnftWAHAkpK0Lre9pmP9WXBOwmlktUbD4SevpffG8dj27w=w2400",
   "https://lh3.googleusercontent.com/cL7-xJkqWgrMG82Pt2et1W5_KQV6Dn7JZPnP2keSBfAcYsygJIJy7CFK-QFxjX1mmyAhshVyZDxC5EDEx2OdrKIPUQn5cqUIwFfoNBbU5rURIA3hf43BbjKdYx3Px0hZKVA0PTojMA=w2400",
-  "https://lh3.googleusercontent.com/aHCDbHfecRCZw5Fa7MyH0pDppUYnNQdHoPMO42ihQBFQMmFkYB43KR5KwO6tztQTUPxydS3JgXL8HhbSKbqPISeIs5Y9oRfQ-J4q4WB2KFO42finzULkwOUeBigbvCu6AHbwgHaU3Q=w2400"
+  "https://lh3.googleusercontent.com/tunhasj6YeyTaUJCEk9Wam_XcZ3MzZpm0mNU3vrK3ClWPlayXEzZt-WwlRGg7r2xsuKE_o94TmyjRDSAlGDN8iDztd1PgfZf6mJ4IHwnPk0BKKmSfuAIGZYO5tKDKmiIQzsUEXbCqQ=w2400"
 ];
   document.body.style.backgroundColor = "#000";
   var ran = Math.random();
